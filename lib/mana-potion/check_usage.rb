edc6f4b0018b7c8847dea8311013498d3d522d70
mrodrigues <mrodrigues.uff@gmail.com>
2015-08-30 00:40:10 -0300

    Extracting service to check usage

---
+++ lib/mana-potion/check_usage.rb
@@ -0,0 +1,30 @@
+module ManaPotion
+  class CheckUsage
+    attr_reader :model, :owner, :limit, :period
+
+    def initialize(model, owner, limit, period)
+      @model = model
+      @owner = owner
+      @limit = limit
+      @period = period
+    end
+
+    def exceeded?
+      count >= limit
+    end
+
+    def association
+      owner
+      .class
+      .reflect_on_all_associations
+      .detect { |r| r.class_name == model.class.name }
+    end
+
+    def count
+      owner
+      .send(association.name)
+      .where(created_at: period.ago..Time.current)
+      .count
+    end
+  end
+end
--- lib/mana-potion/pool.rb
+++ lib/mana-potion/pool.rb
@@ -1,3 +1,5 @@
+require 'mana-potion/check_usage'
+
 module ManaPotion
   module Pool
     def self.included(other)
@@ -10,19 +12,9 @@ module ManaPotion
           limit = instance_exec &limit if limit.respond_to?(:call)
           period = instance_exec &period if period.respond_to?(:call)
 
-          owner = send(association)
-          other_side_association = owner
-          .class
-          .reflect_on_all_associations
-          .detect { |r| r.class_name == self.class.name }
-
-          count = owner
-          .send(other_side_association.name)
-          .where(created_at: period.ago..Time.current)
-          .count
-
-          if count >= limit
-            errors.add(association, :limit, limit: limit, count: count)
+          check_usage = ManaPotion::CheckUsage.new(self, send(association), limit, period)
+          if check_usage.exceeded?
+            errors.add(association, :limit, limit: limit, count: check_usage.count)
           end
         end
       end
